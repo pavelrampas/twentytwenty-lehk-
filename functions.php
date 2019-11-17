94c5e292879607e5c724f69411bfc8b5868b03f6
Pavel Rampas <pavelrampas@gmail.com>
2019-11-17 09:43:19 +0100

    Initial commit

---
+++ functions.php
@@ -0,0 +1,47 @@
+<?php
+
+function my_wp_enqueue_scripts() {
+    wp_enqueue_style( 'twentytwenty-light', get_theme_file_uri( '/style.css' ) );
+
+	// dequeue styles
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'twentytwenty-print-style' );
+	wp_dequeue_style( 'twentytwenty-style' );
+
+	// dequeue scripts
+	wp_dequeue_script( 'twentytwenty-js' );
+	wp_dequeue_script( 'twentytwenty-block-editor-script' );
+	wp_dequeue_script( 'twentytwenty-customize' );
+	wp_dequeue_script( 'twentytwenty-color-calculations' );
+	wp_dequeue_script( 'twentytwenty-customize-controls' );
+	wp_dequeue_script( 'twentytwenty-customize-preview' );
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_dequeue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'my_wp_enqueue_scripts', 11 );
+
+/**
+ * Disable embeds.
+ */
+function disable_embeds_code_init() {
+	if ( ! is_admin() ) {
+		remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+	}
+}
+add_action( 'init', 'disable_embeds_code_init', 11 );
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_action( 'embed_head', 'print_emoji_detection_script' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+}
+add_action( 'init', 'disable_emojis', 11 );
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Twenty Twenty Light
+
+WordPress Twenty Twenty Child Theme without js and css.
+
+### Changes:
+  - Disabled all .js files on frontend.
+  - Disabled all .css files on frontend except base theme style.css file.
+  - Disabled everything about emoji's.
+  - Removed @supports and fonts parts from style.css file.
+  - Minified style.css file. https://www.cleancss.com/css-minify/
